d07b489d883529b725059c29896bf83d387016ce
luoye <fangjian.fj@alibaba-inc.com>
2019-01-29 13:55:08 +0800

    add multi @StreamListener methods & @ServiceActivator methods

--- rocketmq-binder-send-receive-demo/src/main/java/org/springframework/cloud/alibaba/cloud/demo/SendAndReceiveApplication.java
+++ rocketmq-binder-send-receive-demo/src/main/java/org/springframework/cloud/alibaba/cloud/demo/SendAndReceiveApplication.java
@@ -8,7 +8,9 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 
 /**
  * @author <a href="mailto:fangjian0423@gmail.com">Jim</a>
@@ -17,10 +19,19 @@ import org.springframework.integration.support.MessageBuilder;
 @EnableBinding({ Source.class, Sink.class })
 public class SendAndReceiveApplication {
 
+	public static final String TEMP_OUTPUT = "temp-output";
+
+	/**
+	 * message use in ServiceActivatorReceiveService
+	 */
+	@Bean(SendAndReceiveApplication.TEMP_OUTPUT)
+	public MessageChannel tempChannel() {
+		return new DirectChannel();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SendAndReceiveApplication.class, args);
-    }
+	}
 
 	@Bean
 	public CustomRunner customRunner() {
@@ -28,6 +39,7 @@ public class SendAndReceiveApplication {
 	}
 
 	public static class CustomRunner implements CommandLineRunner {
+
 		@Autowired
 		private Source source;
 
@@ -35,7 +47,7 @@ public class SendAndReceiveApplication {
 		public void run(String... args) throws Exception {
 			int count = 5;
 			for (int index = 1; index <= count; index++) {
-                source.output().send(MessageBuilder.withPayload("msg-" + index).build());
+				source.output().send(MessageBuilder.withPayload("msg-" + index).build());
 			}
 		}
 	}
